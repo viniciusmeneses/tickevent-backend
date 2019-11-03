b2abb085f8225b2373d5d488af49283e7c5dcf54
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-11-03 17:05:09 -0300

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const auth = require('json-server-auth');
+
+const app = jsonServer.create();
 const router = jsonServer.router('db.json');
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+app.db = router.db;
 
-server.listen(port);
+app.use(middlewares);
+app.use(auth);
+app.use(router);
+app.listen(port);
